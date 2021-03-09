92846cf70ef6780756257946d8d1948c791698b6
Viva Chan <vivachan29@gmail.com>
2021-03-09 21:44:39 +0800

    fix r

--- app.py
+++ app.py
@@ -56,8 +56,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=))
-
+        TextSendMessage(text=r))
 
 if __name__ == "__main__":
     app.run()
