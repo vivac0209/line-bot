167dc7b8b0791c92d4e47d490c9726cc11b259ee
Viva Chan <vivachan29@gmail.com>
2021-03-09 22:48:44 +0800

    fix

--- app.py
+++ app.py
@@ -50,12 +50,15 @@ def handle_message(event):
     r = '看不懂'
 
 #貼圖回復
-    if msg == '貼圖':
-        ine_bot_api.reply_message(
-            event.reply_token,
-            StickerSendMessage(
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
                 package_id='11537',
-                sticker_id='52002758'))
+                sticker_id='52002758')
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)
+        return              
 
     if msg in ['hi' , 'Hi']:
         r = 'hello'
