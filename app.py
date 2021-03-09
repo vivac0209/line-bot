26a9188c6f571114a7d98efc93f5abc7fe98acc3
Viva Chan <vivachan29@gmail.com>
2021-03-09 22:03:14 +0800

    fix 503 error

--- app.py
+++ app.py
@@ -46,13 +46,13 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	msg = event.message.text
-	r = '看不懂'
+    msg = event.message.text
+    r = '看不懂'
 
-	if msg == 'hi':
-		r = 'hello'
-	elif msg == '吃飯了嗎':
-		r = '還沒'
+    if msg == 'hi':
+        r = 'hello'
+    elif msg == '吃飯了嗎':
+        r = '還沒'
 
     line_bot_api.reply_message(
         event.reply_token,
